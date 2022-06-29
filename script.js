68a0b0b1cf90c3f5bbbd5b739021388d9237c424
Madhu Roshan <95310038+madhuroshan@users.noreply.github.com>
2022-06-29 22:50:23 +0530

    added js file and created a menu button

---
+++ script.js
@@ -0,0 +1,16 @@
+const logoTwo = document.querySelector(".topnav-two");
+const menuButton = document.querySelector(".menu-button");
+const logoOne = document.querySelector(".topnav");
+let screenWidth = window.matchMedia("(max-width:500px)");
+
+screenWidth.addListener(function (screenWidth) {
+  if (screenWidth.matches) {
+    logoTwo.style.display = "grid";
+    menuButton.style.display = "block";
+    logoOne.style.display = "none";
+  } else {
+    logoTwo.style.display = "none";
+    menuButton.style.display = "none";
+    logoOne.style.display = "grid";
+  }
+});
