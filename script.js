592a3ad52246f9aca466c2dc8bc21ea55e0c4252
Madhu Roshan <95310038+madhuroshan@users.noreply.github.com>
2022-06-30 15:31:45 +0530

    completed menu bar in mobile version

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const menuButton = document.querySelector(".menu-button");
 const logoOne = document.querySelector(".topnav");
 const workGrid = document.querySelector(".work-grid");
 let screenWidth = window.matchMedia("(max-width:500px)");
-
+const topnavMenu = document.querySelector(".topnav-links-two");
+const topnavMenuClose = document.querySelector(".close-button");
 screenWidth.addListener(function (screenWidth) {
   if (screenWidth.matches) {
     logoTwo.style.display = "grid";
@@ -13,5 +14,25 @@ screenWidth.addListener(function (screenWidth) {
     logoTwo.style.display = "none";
     menuButton.style.display = "none";
     logoOne.style.display = "grid";
+  }
+});
+
+menuButton.addEventListener("click", function () {
+  if ((topnavMenu.style.display = "none")) {
+    topnavMenu.style.display = "flex";
+    topnavMenuClose.style.display = "block";
+    menuButton.style.display = "none";
+  } else {
+    topnavMenu.style.display = "none";
+  }
+});
+
+topnavMenuClose.addEventListener("click", function () {
+  if ((topnavMenuClose.style.display = "block")) {
+    topnavMenu.style.display = "none";
+    topnavMenuClose.style.display = "none";
+    menuButton.style.display = "block";
+  } else {
+    topnavMenu.style.display = "flex";
   }
 });
