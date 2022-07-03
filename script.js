d5dc0e608b412a78e6d830835cd74321850df199
Madhu Roshan <95310038+madhuroshan@users.noreply.github.com>
2022-07-03 13:55:49 +0530

    added media queries

--- script.js
+++ script.js
@@ -2,21 +2,9 @@ const logoTwo = document.querySelector(".topnav-two");
 const menuButton = document.querySelector(".menu-button");
 const logoOne = document.querySelector(".topnav");
 const workGrid = document.querySelector(".work-grid");
-let screenWidth = window.matchMedia("(max-width:500px)");
+
 const topnavMenu = document.querySelector(".topnav-links-two");
 const topnavMenuClose = document.querySelector(".close-button");
-
-screenWidth.addListener(function (screenWidth) {
-  if (screenWidth.matches) {
-    logoTwo.style.display = "grid";
-    menuButton.style.display = "block";
-    logoOne.style.display = "none";
-  } else {
-    logoTwo.style.display = "none";
-    menuButton.style.display = "none";
-    logoOne.style.display = "grid";
-  }
-});
 
 menuButton.addEventListener("click", function () {
   if ((topnavMenu.style.display = "none")) {
